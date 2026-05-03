37e391b659df1b24cc6f0ebdf2831601c31fef87
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2026-05-03 08:13:10 +0200

    fix the import bug for ProductList in ProductsPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,8 @@
-import ProductList from "../components/ProductList";
 import ReactPaginate from "react-paginate";
 import useGetAllProducts from "../hooks/useGetAllProducts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfinity, faSearch } from "@fortawesome/free-solid-svg-icons";
+import ProductList from "../components/productList";
 export default function ProductsPage() {
   const {
     allProduct,
